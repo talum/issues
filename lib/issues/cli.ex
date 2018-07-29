9b76f3bbe0d83e53811aa002c3b21f2c176694ba
talum <tracy.a.lum@gmail.com>
2018-07-29 10:24:26 -0400

    option parser

---
+++ lib/issues/cli.ex
@@ -0,0 +1,24 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean ], aliases: [ h: :help ])
+
+    case parse do
+      { [ help: true ], _, _ }
+        -> :help
+
+      { _, [ user, project, count ], _ }
+        -> { user, project, String.to_integer(count) }
+
+      { _, [ user, project ], _ }
+        -> { user, project, @default_count }
+
+      _ -> :help
+    end
+  end
+end
