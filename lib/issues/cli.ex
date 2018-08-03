a547fada061705d001e1e87a250eeb8d216f2e76
talum <tracy.a.lum@gmail.com>
2018-08-02 23:00:53 -0400

    add fetching

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,7 +2,40 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 => i1["created_at"] <= i2["created_at"] end
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}), do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from GitHub: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    list
+    |> Enum.map(&Enum.into(&1, HashDict.new))
   end
 
   def parse_args(argv) do
---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,22 @@
+defmodule Issues.GithubIssues do
+  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
+  @github_url Application.get_env(:issues, :github_url)
+
+  def issues_url do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def handle_response({:ok, %HTTPoison.Response{body: body}}) do
+    { :ok, :jsx.decode(body) }
+  end
+
+  def handle_response({_, %HTTPoison.Response{body: body}}) do
+    { :error, :jsx.decode(body) }
+  end
+end
